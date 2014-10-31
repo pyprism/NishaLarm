ec207b26acdee7fa607cf0b999724fe7ad0a1a30
pyprism <git.pyprism@gmail.com>
2014-10-31 21:52:51 +0600

    basic view

--- hiren/alarm/views.py
+++ hiren/alarm/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Api, Nisha
+from .serializers import ApiSerializer, NishaSerializer
 # Create your views here.
+
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+def test(request):
+    if request.method == 'GET':
+        api = Api.objects.all()
+        serializer = ApiSerializer(api)
+        return JSONResponse(serializer.data)
