d46c1e185194ade2ad37468eb662497c44a7c69f
pyprism <git.pyprism@gmail.com>
2014-10-31 21:51:27 +0600

    removed users

--- hiren/alarm/models.py
+++ hiren/alarm/models.py
@@ -2,16 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Api(models.Model):
-	rater = models.ForeignKey(User)
-	account_sid = models.CharField(max_length=50)
-	auth_token = models.CharField(max_length=50)
-	from_no = models.CharField(max_length=30)
-	to_no = models.CharField(max_length=30)
+    account_sid = models.CharField(max_length=50)
+    auth_token = models.CharField(max_length=50)
+    from_no = models.CharField(max_length=30)
+    to_no = models.CharField(max_length=30)
 
 
 class Nisha(models.Model):
-	santo = models.ForeignKey(User)
-	message = models.TextField(null=True, blank=True)
-	time = models.DateTimeField()
-	interval = models.DateTimeField()
+    message = models.TextField(null=True, blank=True)
+    time = models.DateTimeField()
+    interval = models.DateTimeField()
