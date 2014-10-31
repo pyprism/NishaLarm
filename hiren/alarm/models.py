f94eb53cb95a301809ae46372e13f0748322541d
pyprism <areose21@gmail.com>
2014-10-31 13:08:26 +0600

    added model data

--- hiren/alarm/models.py
+++ hiren/alarm/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Api(models.Model):
+	rater = models.ForeignKey(User)
+	account_sid = models.CharField(max_length=50)
+	auth_token = models.CharField(max_length=50)
+	from_no = models.CharField(max_length=30)
+	to_no = models.CharField(max_length=30)
+
+
+class Nisha(models.Model):
+	santo = models.ForeignKey(User)
+	message = models.TextField(null=True, blank=True)
+	time = models.DateTimeField()
+	interval = models.DateTimeField()
