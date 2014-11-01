00c61a5d9467e5d2642e7f55a995db951619d2f2
pyprism <git.pyprism@gmail.com>
2014-11-01 23:01:21 +0600

    added working url

--- hiren/hiren/urls.py
+++ hiren/hiren/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
+from alarm import views
+
+
+router = routers.DefaultRouter()
+router.register(r'api', views.ApiViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,5 +13,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^test/', 'alarm.views.test')
+    url(r'^test/', 'alarm.views.test'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
