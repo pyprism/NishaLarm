3544360b4d100d66adac91b98eb35e4afa8deca1
Ashutosh Das <git.pyprism@gmail.com>
2016-10-27 20:49:12 +0600

    Added model

--- alarm/models.py
+++ alarm/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Api(models.Model):
+    account_sid = models.CharField(max_length=50)
+    auth_token = models.CharField(max_length=50)
+    from_number = models.CharField(max_length=30)
+    to_number = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.account_sid)
+
+
+class Nisha(models.Model):
+    time = models.DateField()
+    snooze = models.BooleanField(default=False)
+    repeat_time = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.time, self.done
